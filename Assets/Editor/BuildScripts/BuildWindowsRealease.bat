69e044779b4cbbbb0324c8515c2b71dc276a06c4
Pedro Henrique <113391251+PedroPontesRay@users.noreply.github.com>
2024-09-23 15:31:11 -0300

    BuildWindows bat added

---
+++ Assets/Editor/BuildScripts/BuildWindowsRealease.bat
@@ -0,0 +1,3 @@
+@ECHO
+
+"C:\Program Files\Unity\Hub\Editor\2022.3.16f1\Editor\Unity.exe" -quit -batchMode -projectPath "C:\Users\thera\Projeto\JenkinsUnity" -executeMethod BuildScript.BuildWindowsRealease
