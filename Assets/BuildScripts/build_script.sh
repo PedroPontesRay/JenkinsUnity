0b69f302d66e8279f27af689fa1cc275eb70b6b9
Pedro Henrique <113391251+PedroPontesRay@users.noreply.github.com>
2024-10-11 16:57:33 -0300

    Add Debug To test if the build is debug

--- Assets/BuildScripts/build_script.sh
+++ Assets/BuildScripts/build_script.sh
@@ -1,6 +1,5 @@
 echo "[BUILD SCRIPT TESTE] Iniciando a build no ambiente configurado"
 mkdir -p "output"
-echo "[BUILD SCRIPT TESTE] Build concluída e armazenada em 'output'"
 
 "C:/Program Files/Unity/Hub/Editor/2022.3.16f1/Editor/Unity.exe" -quit -batchMode -projectPath "C:/Users/thera/Projeto/JenkinsUnity" -executeMethod BuildScript.BuildWindowsDev
 
