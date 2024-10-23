81cf5072cf2c11170cf4e44e1b83e47f985066c8
PedroPontesRay <pedrohenripn@gmai.com>
2024-10-23 17:09:02 -0300

    Test new path

--- Assets/BuildScripts/BuildWindowsDev.bat
+++ Assets/BuildScripts/BuildWindowsDev.bat
@@ -1,3 +1,3 @@
 @ECHO
 
-"C:\Program Files\Unity\Hub\Editor\2022.3.16f1\Editor\Unity.exe" -quit -batchMode -projectPath "C:\Users\thera\Projeto\JenkinsUnity" -executeMethod BuildScript.BuildWindowsDev
+"C:\Program Files\Unity\Hub\Editor\2022.3.16f1\Editor\Unity.exe" -quit -batchMode -projectPath "D:\Projetos\JenkinsUnity -executeMethod BuildScript.BuildWindowsDev
