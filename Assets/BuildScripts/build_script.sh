6564bdd9a7083dd423ab30817204fa7fa425ad66
PedroPontesRay <pedrohenripn@gmai.com>
2024-10-23 16:35:34 -0300

    Cahnge the name to projetos

--- Assets/BuildScripts/build_script.sh
+++ Assets/BuildScripts/build_script.sh
@@ -1,6 +1,6 @@
 echo "[BUILD SCRIPT TESTE] Iniciando a build no ambiente configurado"
 mkdir -p "output"
 
-"C:/Program Files/Unity/Hub/Editor/2022.3.16f1/Editor/Unity.exe" -quit -batchMode -projectPath "C:/Users/thera/Projeto/JenkinsUnity" -executeMethod BuildScript.BuildWindowsDev
+"C:/Program Files/Unity/Hub/Editor/2022.3.16f1/Editor/Unity.exe" -quit -batchMode -projectPath "C:/Users/thera/Projetos/JenkinsUnity" -executeMethod BuildScript.BuildWindowsDev
 
 echo "[Debug.BUILD SCRIPT] Build process completed successfully"
